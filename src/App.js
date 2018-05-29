55cba75267bd167a8a03601d290731d0151cc0ec
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-29 15:19:06 -0400

    Tie value to state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Route, Switch,NavLink} from 'react-router-dom';
 import Github from './Github'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
